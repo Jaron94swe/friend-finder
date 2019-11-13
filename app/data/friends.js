858bd931c309b267ad6b785bb82d0df7eb76ed12
Sven Larsson <ejlandeen@gmail.com>
2019-11-12 20:28:56 -0700

    added htmlRoutes.js and updated the server file to require the folder.

---
+++ app/data/friends.js
@@ -0,0 +1,44 @@
+var friendsArray = [{
+    name: "Micheal Westen",
+    photo: "https://a1cf74336522e87f135f-2f21ace9a6cf0052456644b80fa06d4f.ssl.cf2.rackcdn.com/images/characters_opt/p-burn-notice-jeffrey-donovan.jpg",
+   gender: "Male",
+    scores: [5, 5, 3, 4, 5, 5, 5, 4, 5, 5]
+}, {
+    name: "Sam axe",
+    photo: "https://i.pinimg.com/originals/6b/bf/4c/6bbf4c25f5699bf3303984c14d71921e.jpgg",
+    gender: "Male",
+    scores: [5, 4, 2, 2, 5, 5, 3, 4, 3, 2]
+},
+{
+    name: "Fiona Glenanne",
+    photo: "https://vignette.wikia.nocookie.net/burnnotice/images/6/66/Burn-notice-fiona.jpg/revision/latest?cb=20121103071136",
+    gender: "Female",
+    scores: [5, 5, 4, 1, 5, 5, 4, 5, 5, 1]
+},
+{
+    name: "Nathon Westen",
+    photo: "https://vignette.wikia.nocookie.net/burnnotice/images/f/fd/Nate-westen.jpg/revision/latest?cb=20130101024937",
+    gender: "Male",
+    scores: [4, 3, 3, 2, 3, 5, 3, 3, 3, 3]
+},
+{
+    name: "Madeline Westen",
+    photo: "https://static.tvgcdn.net/mediabin/galleries/shows/a_f/bq_bz/burn_notice/season5/burn-notice-229.jpg",
+    gender: "Female",
+    scores: [4, 3, 4, 1, 2, 5, 4, 5, 4, 2]
+},
+{
+    name: "Jesse porter",
+    photo: "https://vignette.wikia.nocookie.net/burnnotice/images/6/69/Coby_bell.jpg/revision/latest?cb=20170921173224",
+    gender: "Male",
+    scores: [5, 4, 4, 3, 5, 5, 4, 4, 5, 3]
+},
+{
+    name: "Barry Burkowski",
+    photo: "https://i.pinimg.com/originals/fe/1e/2a/fe1e2a20af36ff4f9ce1531672cc0356.png",
+    gender: "Male",
+    scores: [3, 2, 3, 4, 3, 4, 3, 3, 3, 3]
+}
+];
+
+module.exports = friendsArray;
